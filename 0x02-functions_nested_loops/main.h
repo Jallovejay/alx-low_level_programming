d221191219a9490e4dfcc3a87bd3085113e31001
Jallovejay <jackiemurithi93@gmail.com>
2023-06-27 02:39:04 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-#ifndef man_h
-#define man_h
+#ifndef MAIN_H_
+#define MAIN_H_
 
-int_putchar[char];
+int _putchar(char c);
+void print(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -11,6 +12,9 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 void print_to_98(int n);
-
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 #endif
