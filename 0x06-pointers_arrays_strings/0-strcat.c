c5318b1210078c0b9e8c78f4865c322e2c8cc480
Jallovejay <jackiemurithi93@gmail.com>
2023-07-18 21:33:28 -0700

    #include main.h

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,43 +1,29 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
  * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
-int a = -1, i;
-for (i = 0; dest[i] != '\0'; i++)
-;
+	int i;
+	int j;
 
-do {
-	a++;
-	dest[i] = src[a];
-	i++;
-} while (src[a] != '\0');
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-return (dest);
+	dest[i] = '\0';
+	return (dest);
 }
-
-Task 1
-
-#include "main.h"
-#include <stdio.h>
-/**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
- */
-
-char *_strncat(char *dest, char *src, int n)
-{
-
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-;
-
